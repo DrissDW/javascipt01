78c1af023af954dce86f44126b39aa8e118ec2c8
DrissDW <driss.mallali@yahoo.com>
2022-06-27 16:46:09 +0200

    exo 3

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ SUCRE=80;
 
 console.error(("nous avons du sel.") + SEL +("nous avons du sucre.") + SUCRE);
 TEMP= SEL;
-SEL=SUCRE;
+SEL= SUCRE;
 SUCRE=TEMP;
 console.error(("nous avons du sel.") + SEL +("nous avons du sucre.") + SUCRE);
+
+
+
+age = prompt("quel est votre age?");
+console.info("l'utilisateur a"+ age + "ans");
+e= parseInt(age)+2
+console.info("lutilisateur aura"+ e + "ans dans 2 ans");
+
+prix= prompt("quel est le prix de cet article?");
+
+console.info("l'article vaut"+ prix + "euros");
+prixttc= parseInt(prix)*1.2;
+console.info("l'article vaut"+ prixttc + "euros TTC");
