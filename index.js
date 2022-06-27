d8e7cb1aee2e6557a7c883822bc315dad18ab52c
DrissDW <driss.mallali@yahoo.com>
2022-06-27 14:24:04 +0200

    exo2

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ nous allons apprendre
 
 
 */
+a=10
+b=50
+c= "80"
+b=90
 
+console.error(a+b);
+console.error(typeof (a));
+console.error(b);
+console.error(a+parseInt(c));
 
-
-
-console.error("test");
+SEL=100;
+SUCRE=80;
+console.error("nous avons du sel."+ SEL + ".nous avons du sucre"+ SUCRE);
