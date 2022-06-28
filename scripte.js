312987154a57fce7bbeb68d8af89a51225fc7d6a
DrissDW <driss.mallali@yahoo.com>
2022-06-28 13:31:58 +0200

    exo b oucle

--- scripte.js
+++ scripte.js
@@ -34,3 +34,62 @@ if(valeur>0){
 else{
     console.info( "le nombre est negatif");
 }
+
+monprenom="Driss";
+prenom=prompt("Entrer votre Prénom");
+
+if(prenom==monprenom)
+{
+    console.info(" cool vous avez le même prénom que moi");
+
+}
+else{25
+
+
+    console.info("... " );
+}
+
+//for  (i=0;i<5;i++){
+    alert("test");//
+//
+
+for (i=1;i<101;i++)
+console.info(i);
+
+for(f=100;f<201;f++)
+console.info(f);
+
+/*for (g=0;g<10;g++)
+console.info("2*"+ g +"="*2);*/
+
+for (h=0;h<11;h++){
+console.info("table de"+ h);
+
+for (j=1;j<10;j++)
+console.info(h +"*"+j+"="+h*j);
+
+
+
+
+
+
+}
+
+for(l=1;l<11;l++){
+    console.info( "table addition de_"+ l)
+for(m=1;m<11;m++)
+console.info(l+"+"+m+"="+(l+m));
+
+
+}
+
+for(x=0;x<101;x++)
+{
+console.info(x+"*");
+
+}
+
+for(y=1;y<6;y++){
+
+    console.info(y+"*****");
+}
